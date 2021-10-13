242b55a8e460d6eecb5532be4ca01db293fb7dc8
Maor Kadosh <git@avocadosh.xyz>
2021-10-13 16:36:04 +0300

    fix commentary tag for MELPA

--- eldoc-toml.el
+++ eldoc-toml.el
@@ -11,7 +11,6 @@
 ;; This file is not part of GNU Emacs.
 ;;
 ;;; Commentary:
-;;
 ;; An ElDoc function for displaying the parent table of the field at point in a TOML file, along the
 ;; key name. Makes it easier to navigate tables with many fields, where you might have the table’s
 ;; header or the key name off screen while still navigating its fields.
