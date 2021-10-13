8123b5d0fb876de3e38474d517e41aa6a7d3a9d4
Maor Kadosh <git@avocadosh.xyz>
2021-10-13 13:55:12 +0300

    rebrand to eldoc-toml

--- eldoc-toml.el
+++ eldoc-toml.el
@@ -1,9 +1,9 @@
-;;; eldoc-toml-current-table.el --- TOML table name at point for ElDoc -*- lexical-binding: t; -*-
+;;; eldoc-toml.el --- TOML table name at point for ElDoc -*- lexical-binding: t; -*-
 ;;
 ;; Copyright (C) 2020 Ma'or Kadosh
 ;;
 ;; Author: Ma'or Kadosh <git@avocadosh.xyz>
-;; URL: https://github.com/it-is-wednesday/eldoc-toml-current-table
+;; URL: https://github.com/it-is-wednesday/eldoc-toml
 ;; Version: 0.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: data
@@ -17,12 +17,12 @@
 ;;
 ;;; Code:
 
-(defun eldoc-toml-current-table--remove-comment (line)
+(defun eldoc-toml--remove-comment (line)
   "Remove comment from end of LINE.
 Also remove any space between the actual line's ending to the comment's beginning."
   (replace-regexp-in-string "\s*#.*" "" line))
 
-(defun eldoc-toml-current-table--current-table-name ()
+(defun eldoc-toml--current-table-name ()
   "Find which table the current line is under.
 For example, in this scenario, where X is the cursor:
 
@@ -42,22 +42,22 @@ returns [a]."
     (goto-char initial-point)
 
     ;; Trimming since TOML lines can begin with whitespace
-    (string-trim (eldoc-toml-current-table--remove-comment current-line))))
+    (string-trim (eldoc-toml--remove-comment current-line))))
 
-(defun eldoc-toml-current-table--callback (callback &rest _more)
+(defun eldoc-toml--callback (callback &rest _more)
   "Document the table the value at point is in and pass it to CALLBACK.
 Add this to `eldoc-documentation-functions'."
-  (let ((table (eldoc-toml-current-table--current-table-name)))
+  (let ((table (eldoc-toml--current-table-name)))
     ;; make sure we got some table value, because some TOML documents begin with comments - in
     ;; which case we should just be quiet.
     (when table
       (funcall callback (concat "In table: " table)))))
 
 ;;;###autoload
-(defun eldoc-toml-current-table ()
+(defun eldoc-toml ()
   "Add the hook to `eldoc-documentation-functions'."
   (interactive)
-  (add-hook 'eldoc-documentation-functions #'eldoc-toml-current-table--callback nil 'local))
+  (add-hook 'eldoc-documentation-functions #'eldoc-toml--callback nil 'local))
 
-(provide 'eldoc-toml-current-table)
-;;; eldoc-toml-current-table.el ends here
+(provide 'eldoc-toml)
+;;; eldoc-toml.el ends here
